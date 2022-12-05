035e1742fc79c1ac218b804f1481fa30702f6857
Oscar_kng <Oscarkanangila01@gmail.com>
2022-12-05 23:41:49 +0200

    api ok

--- main.js
+++ main.js
@@ -1,61 +1,41 @@
-//Tableau des citation il est preferable
-// d'utiliser un api a la place
-
-
-const key = 'UktKYumO0YYAw/8y/dTqfQ==eFo06AMRTUHua1MH';
-const category = 'computers';
+const key = "UktKYumO0YYAw/8y/dTqfQ==eFo06AMRTUHua1MH";
+const category = "computers";
 
 const url = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
 
-
 const headers = new Headers();
 
-const request = new Request(url,{
-    method:'GET',
-    headers:{ 'X-Api-Key': key},
-    mode:'cors',
-    cache:'default',
-
+const request = new Request(url, {
+  method: "GET",
+  headers: { "X-Api-Key": key },
+  mode: "cors",
+  cache: "default",
 });
 
-fetch(request)
-    .then((response)=>response.json())
-    .then(data=>{
-        console.log(data[0]);
-        display_data(data)
-    })
-
-
-
-
-
 //selection des elements html
-let btn = document.getElementById('btn');
-let citation =  document.getElementById('citation');
-let categorie = document.getElementsByClassName('categorie')
-let auteur = document.getElementsByClassName('auteur');
+let btn = document.getElementById("btn");
 
-function display_data(data){
-    const quote = data[0].quote;
-    const category = data[0].category;
-    const author = data[0].author;
+let quote = document.getElementById("quote");
+let cat = document.getElementById("cat");
+let aut = document.getElementById("aut");
+function display_data(data) {
+  const quotation = data[0].quote;
+  const category = data[0].category;
+  const author = data[0].author;
 
-    //display data
+  //display data
 
-    citation.innerHTML = quote;
-    categorie.innerHTML = category;
-    auteur.innerHTML = author;
+  quote.innerHTML = quotation;
+  cat.innerHTML = category;
+  aut.innerHTML = author;
 }
 
-// //chargement au click
-// btn.addEventListener('click',()=>{
-//     let aleatoire = Math.floor(Math.random()*(quotesBd.length));
-//     afficher.innerHTML = quotesBd[aleatoire];
-// })
-
-
 // //chargement automatique apres 3s
-// setInterval(()=>{
-//     let aleatoire = Math.floor(Math.random()*(quotesBd.length));
-//     afficher.innerHTML = quotesBd[aleatoire];
-// }, 5000);
+setInterval(() => {
+  fetch(request)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0]);
+      display_data(data);
+    });
+}, 5000);
